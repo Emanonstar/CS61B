bbb0977ee946450f75a9da9378319eb60a44071f
Emanon <emanonstar@gmail.com>
2022-03-25 21:51:06 +0800

    add tests for different case

--- proj1b/TestOffByN.java
+++ proj1b/TestOffByN.java
@@ -8,10 +8,13 @@ public class TestOffByN {
     @Test
     public void testEqualChars() {
         assertTrue(offBy5.equalChars('a', 'f'));
+        assertTrue(offBy5.equalChars('A', 'F'));
         assertTrue(offBy5.equalChars('f', 'a'));
         assertTrue(offBy5.equalChars('o', 't'));
         assertFalse(offBy5.equalChars('a', 'e'));
         assertFalse(offBy5.equalChars('z', 'a'));
         assertFalse(offBy5.equalChars('a', 'a'));
+        assertFalse(offBy5.equalChars('a', 'F'));
+        assertFalse(offBy5.equalChars('A', 'f'));
     }
 }
